e49812d96438c6b4b3719b015e7986bab8b7ff8e
quycute279 <quyykvph52340@gmail.com>
2025-06-27 23:19:19 +0700

    27_6_quy

---
+++ DATN_SD_102/Repository/IRepository/IDanhMucLinhKienThuocTinhRepository.cs
@@ -0,0 +1,13 @@
+﻿using F4_API.Models;
+
+namespace F4_API.Repository.IRepository
+{
+    public interface IDanhMucLinhKienThuocTinhRepository
+    {
+        Task<IEnumerable<DanhMuc_LinhKien_ThuocTinh>> GetAllAsync();
+        Task<DanhMuc_LinhKien_ThuocTinh?> GetByIdAsync(Guid id);
+        Task AddAsync(DanhMuc_LinhKien_ThuocTinh entity);
+        Task UpdateAsync(DanhMuc_LinhKien_ThuocTinh entity);
+        Task DeleteAsync(Guid id);
+    }
+}
