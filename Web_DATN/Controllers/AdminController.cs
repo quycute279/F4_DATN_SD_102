019ff22cc4525c37c51f49a51983faeb0075b3b4
quycute279 <quyykvph52340@gmail.com>
2025-07-02 15:10:40 +0700

    them_LK_Admin_controll

---
+++ Web_DATN/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_DATN.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            var role = HttpContext.Session.GetString("Role");
+            if (role != "Admin")
+                return RedirectToAction("AccessDenied", "TaiKhoan");
+
+            return View(); // Trang quản trị
+        }
+    }
+}
